07c10068a14534f498f7300e675cfb486960631d
nncl <nncl@live.com>
2014-10-12 23:01:47 -0300

    Novo atual

---
+++ src/js/menu.js
@@ -0,0 +1,11 @@
+			function mostrar(){								
+				document.getElementById("nav").style.display = "block";				
+				document.getElementById("close").style.display = "block";
+				document.getElementById("btn").style.display = "none";
+			}
+
+			function fechar(){				
+				document.getElementById("nav").style.display = "none";				
+				document.getElementById("close").style.display = "none";
+				document.getElementById("btn").style.display = "block";
+			}
